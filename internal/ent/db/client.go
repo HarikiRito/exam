80fd03eef02c9fc39ceabea2e610f4344b10b15f
Hariki <nxh0809@gmail.com>
2025-04-01 00:02:36 +0700

    Move ent folder

---
+++ internal/ent/db/client.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"fmt"
+	"template/internal/ent"
+	_ "template/internal/ent/runtime"
+	"template/shared/environment"
+)
+
+func OpenClient() (*ent.Client, error) {
+	return ent.Open("postgres", connectionString())
+}
+
+func connectionString() string {
+	return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", environment.DB_HOST, environment.DB_PORT, environment.DB_USER, environment.DB_NAME, environment.DB_PASSWORD)
+}
---
+++ internal/ent/db/database.go
@@ -0,0 +1,24 @@
+package db
+
+import (
+	"context"
+	"log"
+	"template/internal/ent"
+
+	_ "github.com/lib/pq"
+)
+
+var DbClient *ent.Client
+
+func InitDatabase() {
+	client, err := OpenClient()
+	if err != nil {
+		log.Fatalf("failed opening connection to postgres: %v", err)
+	}
+	defer client.Close()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+}
