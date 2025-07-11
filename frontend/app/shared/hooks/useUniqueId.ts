09ca1bb9e0810c01077f92cc1c5b539c8212a33c
Hariki <nxh0809@gmail.com>
2025-07-11 09:08:56 +0700

    fix lint error

--- frontend/app/shared/hooks/useUniqueId.ts
+++ frontend/app/shared/hooks/useUniqueId.ts
@@ -1,19 +1,15 @@
 import { useCallback, useState } from 'react';
-
+import { v4 as uuidv4 } from 'uuid';
 /**
  * @description Generate a unique ID for a component key to tell react to re-render the component
  *
  * @returns A unique ID
  */
 export function useUniqueId() {
-  const [id, setId] = useState(generateId());
-
-  function generateId() {
-    return crypto.randomUUID() as string;
-  }
+  const [id, setId] = useState(uuidv4());
 
   const regenerate = useCallback(() => {
-    setId(generateId());
+    setId(uuidv4());
   }, []);
 
   return [id, regenerate] as const;
